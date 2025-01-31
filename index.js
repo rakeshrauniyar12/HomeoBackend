ade5807ec9c71344e55cb1519982cc02f49fe0f4
fastcodingx <rakeshrauniyara1234@gmail.com>
2025-01-31 11:48:22 +0530

    setup complete

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+connectDB();
+
+
+app.use(cookieParser());
+app.use(express.json());
+
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+  ],
+  method: "GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD",
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+// Root endpoint
+app.get("/", (req, res) => {
+  res.send("Welcome to the authentication API");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
