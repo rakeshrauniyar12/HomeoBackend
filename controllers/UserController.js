f1ea22041eb94b2f535f043ba527cbcd3578bea9
fastcodingx <rakeshrauniyara1234@gmail.com>
2025-03-22 17:39:08 +0530

    domain url added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -43,12 +43,12 @@ const loginUser = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Email not found." });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Invalid Password" });
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
